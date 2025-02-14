8cf6dcea188f30a890f1d9386ba9580e460cceb5
Ayub Ali <pltsummary@gmail.com>
2025-02-14 12:26:25 -0700

    togglestatus

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"net/http"
-  "errors",
+
 	"github.com/gin-gonic/gin"
 )
+
 type Todo struct {
     ID        string `json:"id"`         // Unique id
     Item      string `json:"item"`       // Task desc
@@ -40,22 +42,56 @@ func addTodo(context *gin.Context){
   context.IndentedJSON(http.StatusCreated, newTodo)
 }
 
-// iteratre through the array and find specific todo
-func getTodoById(id string)(*todo, error){
-  for i, t:= range todos{
+// iterate through the array and find specific todo
+func getTodoById(id string) (*Todo, error) { 
+  for i, t := range todos {
     if t.ID == id {
-        return &todo[i], nil
+        return &todos[i], nil
     }
   }
 
   return nil, errors.New("todo not found")
 }
 
+// function for handler, extracting dynamic ID from the URL
+
+func getTodo(context *gin.Context){
+
+  id := context.Param("id")
+  todo, err := getTodoById(id) 
+
+  if err != nil {
+      context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not Found"})
+      return
+  }
+
+  context.IndentedJSON(http.StatusOK, todo)
+}
+
+
+func toggleTodoStatus(context *gin.Context){
+
+  id := context.Param("id")
+  todo, err := getTodoById(id) 
+
+  if err != nil {
+      context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not Found"})
+      return
+  }
+
+  todo.Completed = !todo.Completed // flip boolean value
+
+
+  context.IndentedJSON(http.StatusOK, todo)
+
+}
 
 // Server Creation & Endpoint
 func main() {
   router := gin.Default()         // Create a new router
-  router.GET("/todos", getTodos)  // Endpoint Creation
+  router.GET("/todos", getTodos)
+  router.GET("/todos/:id", getTodo)  // Endpoint Creation -> stored as id
+  router.PATCH("/todos/:id", toggleTodoStatus)  // Endpoint Creation -> stored as id
   router.POST("/todos", addTodo)
   router.Run("localhost:9090")    
 }
