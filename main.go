7e54372b98073a876fb3465b45a254114a50ba96
Ayub Ali <pltsummary@gmail.com>
2025-02-14 11:33:30 -0700

    server setup

--- main.go
+++ main.go
@@ -1,2 +1,21 @@
 package main
 
+type todo struct {
+    id string `json:"id"`
+    item string `json:"title"`
+    completed bool `json: "completed"`
+}
+
+var todos = []todo{
+    {id: "1", item: "Clean Room", completed: false },
+    {id: "2", item: "Read Book", completed: false },
+    {id: "3", item: "Record Video", completed: false },
+}
+
+// Diff struc that JSON
+
+// Client & the Server communicating through JSON
+// Server -> Sending to Client convert the DS to JSON & vice versa 
+
+
+// Server Creation
